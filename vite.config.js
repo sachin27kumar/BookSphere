c712295e64b85d48f2d59b99702fe1a3bda3bbf9
sachin27kumar <sachin27sheoran@gmail.com>
2024-11-14 15:00:49 +0530

    package file updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Book-Sphere/', // Adjust to your repo name, if it is "todo-app"
+});
+
